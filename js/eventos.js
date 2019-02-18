15a828389ade4e8641741b779869085d340073db
Elton Rodrigues <30576148+Eltonrod@users.noreply.github.com>
2019-02-18 14:53:17 -0300

    Add files via upload

--- js/eventos.js
+++ js/eventos.js
@@ -1,25 +1,21 @@
-<html>
-<body>
-<img id="teste" src="codigoHTML.jpg" width="160" height="120" onLoad="trocaImg();">
 
-<script>
-var num = 2;
-var img = document.getElementById("teste");
 function trocaImg(){
 //apenas para cunho de testes
 setTimeout(function () {
   if (num == 1)
     {
-      img.src = "codigo.jpg";
+      img.src = "img/whatsCor.png";
     }
   else if (num == 2)
     {
-      img.src = "codigoHTML.jpg";
+      img.src = "img/whats.png";
     }
     //garante que num fique alternando entre 1 e 2
     num = (num % 2) + 1;
-    }, 2500);
+  }, 1700);
 }
-</script>
-</body>
-</html>
+
+function aviso(){
+  alert("Olá, só quero avisar que o site está em construção ainda.");
+ document.getElementById('manu').innerHTML="O site está em processo de construção";
+}
